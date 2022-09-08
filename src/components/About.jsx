753daf8341eeca1bc9cfb8fc7a116d19d9692a15
Yumeng Molly Sun <yumengsun@Yumengs-MacBook-Pro.local>
2022-09-08 19:47:19 +0200

    added animation for arrow and new background image and color

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,25 +3,30 @@ import { Link } from "react-scroll";
 import styles from './About.module.css'
 import pic from './IMG_0005.JPG'
 import { IoIosArrowDown } from 'react-icons/io'
+import sun from './sun.png'
 
 const About = () => {
     return (
         <section className="about">
-            <main className={styles.container}>
-                <p className={styles.aboutMe}>About me</p>
-                <p className={styles.text}>
-                    I am a passionate and dedicated developer with an
-                    eagerness to learn. My background in Economics and
-                    data science has equipped me with an analytical
-                    mindset and empirical research ability. I enjoy
-                    problem-solving when in a team as well as
-                    individually, and I am always ready for new
-                    challenges!
-                </p>
-                <img src={pic} className={styles.img} alt="Molly"/>
+            <main className={`${styles.container} ${styles.active} ${styles.fadebottom}`}>
+                <p className={styles.aboutMe}>About me<img src={sun} alt='sun' className={styles.sunImg}/></p>
+                <div className={styles.borderContainer}>
+                    <p className={styles.text}>
+                        I am a passionate and dedicated developer with an
+                        eagerness to learn. My background in Economics and
+                        data science has equipped me with an analytical
+                        mindset and empirical research ability. I enjoy
+                        problem-solving when in a team as well as
+                        individually, and I am always ready for new
+                        challenges!
+                    </p>
+                    <img src={pic} className={styles.img} alt="Molly" />
+                </div>
+                <div className={styles.arrowWrapper}>
                 <Link to="techskill" spy={true} smooth={true}>
                     <IoIosArrowDown className={styles.arrow} />
                 </Link>
+                </div>
             </main>
         </section>
     )
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,21 +3,23 @@ import Navbar from './Navbar';
 import styles from './Intro.module.css'
 import { IoIosArrowDown } from 'react-icons/io'
 import { Link } from "react-scroll";
+import sun from './sun.png'
+
 
 const Intro = () => {
     return (
         <section className="intro">
-        <main className={styles.container}>
-            <Navbar />
-            <header className={styles.header}>
-                <p className={styles.name}>Yumeng Molly Sun</p>
-                <p className={styles.dev}>Full-stack <span className={styles.js}>Javascript</span> developer based in Oslo</p>
-                <p className={styles.more}>Know more about me</p>
-                <Link to="about" spy={true} smooth={true}>
-                    <IoIosArrowDown className={styles.arrow} />
-                </Link>
-            </header>
-        </main>
+            <main className={styles.container}>
+                <Navbar />
+                <header className={styles.header}>
+                    <p className={styles.name}>Yumeng Molly <span className={styles.sun}>Sun</span><img src={sun} alt='sun' className={styles.sunImg}/></p>
+                    <p className={styles.dev}>Full-stack <span className={styles.js}>Javascript</span> developer based in Oslo</p>
+                    <p className={styles.more}>Know more about me</p>
+                    <Link to="about" spy={true} smooth={true}>
+                        <IoIosArrowDown className={styles.arrow} />
+                    </Link>
+                </header>
+            </main>
         </section>
     )
 }
