83c34f225864780b88d16fa123af6174ffbfe5eb
Yumeng Molly Sun <yumengsun@Yumengs-MacBook-Pro.local>
2022-09-21 21:06:27 +0200

    fix mobile view arrow in intro

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () => {
     return (
         <nav className={styles.nav}>
             <Link to="about" spy={true} smooth={true}>
-                <div className={styles.about}>about me</div>
+                <div className={styles.about}>About Me</div>
             </Link>
             <Link to="projects" spy={true} smooth={true}>
-            <div className={styles.works}>my works</div>
+            <div className={styles.works}>My Works</div>
             </Link>
         </nav>
     )
